f03bd18265b472a6c6961c9508069d5229bd5d33
Adetunji Samuel <samadet0001@gmail.com>
2026-06-16 04:46:12 +0100

    Update Twitter link in Footer component

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -22,7 +22,7 @@ export default function Footer() {
             <div className='flex gap-4'>
               {[
                 
-                { Icon: FaXTwitter, href: 'https://x.com/dealcraft_ai?s=21' }
+                { Icon: FaXTwitter, href: 'https://x.com/Pitchcraftt_ai' }
               ].map(({ Icon, href }, i) => (
                 <motion.a
                   key={i}
